14c1fcdbf9b950fecafe7f4f28d8c45272a4f280
DurgeshSChauhan007 <131304057+DurgeshSChauhan007@users.noreply.github.com>
2024-10-19 20:04:10 +0530

    Add files via upload

---
+++ Event Listener /3.mouseOver/script.js
@@ -0,0 +1,7 @@
+const boxElement = document.getElementById("box");
+
+// add a mouseover event listener to the element
+boxElement.addEventListener("mouseover", function() {
+    alert("you entered in Restricted Area 🙅 (mouseover event is working properly)"
+    );
+});
